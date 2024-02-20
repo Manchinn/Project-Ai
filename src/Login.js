3474929a02f9f57fad865353b561e89a8806ff8e
Manchinn <chinnakrit150@gmail.com>
2024-02-20 20:10:17 +0700

    first commit

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,15 @@
-import React from 'react'
-import Button from '@mui/material/Button';
+import React from 'react';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 function Login() {
+  const navigate = useNavigate()
   const [inputs, setInputs] = useState({});
+
+  const MySwal = withReactContent(Swal)
 
   const handleChange = (event) => {
     const name = event.target.name;
@@ -13,8 +19,44 @@ function Login() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(inputs);
-  }
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      "username": inputs.username,
+      "password": inputs.password,
+      "expiresIn": 60000
+    });
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    fetch("https://www.melivecode.com/api/login", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result)
+        if(result.status === 'ok'){
+          MySwal.fire({
+            html: <i>result.message</i>,
+            icon: 'success',
+          }).then((value) => {
+            localStorage.setItem('token', result.accessToken)
+            navigate('/Profile')
+          })
+        } else{
+          MySwal.fire({
+            html: <i>{result.message}</i>,
+            icon: 'error',
+          })
+        }
+      })
+      .catch(error => console.log('error', error));
+        console.log(inputs);
+      }
 
   return (
     <div>
